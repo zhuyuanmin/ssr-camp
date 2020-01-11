a4c93034ac3eca84423645d3b25023d00ce1b44f
zhuyuanmin <13058039407@163.com>
2020-01-11 21:57:22 +0800

    Create Index.jsx

---
+++ src/pages/Index.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { getIndexList } from '../store/index'
+
+function Index(props) {
+  const [count, setCount] = useState(1)
+  useEffect(() => {
+    if (props.list.length === 0) {
+      props.getIndexList()
+    }
+  }, [])
+  return (
+    <div>
+      <h1>哈喽 {props.title} {count}!</h1>
+      <button onClick={() => setCount(count + 1)}>累加</button>
+      <hr />
+      <ul>
+        {
+          props.list.map(item => {
+            return <li key={item.id}>{item.name}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+Index.loadData = store => {
+  return store.dispatch(getIndexList())
+}
+
+export default connect(
+  state => ({ list: state.index.list }),
+  { getIndexList }
+)(Index)
