fae39f35902eec1aa2aa4483f9eb99269a2d292d
zhuyuanmin <13058039407@163.com>
2020-01-11 21:42:48 +0800

    Create webpack.server.js

---
+++ webpack.server.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+// 服务端的webpack
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './server/index.js',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['isomorphic-style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }]
+      }
+    ]
+  }
+}
